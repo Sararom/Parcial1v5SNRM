932f021d81e24bd6f1f1056d204c47ee91649993
Sara <00030716@uca.edu.sv>
2018-05-05 13:45:12 -0600

    5a Version del parcial, en el initial commit hay implementadas muchos recursos que han sido extraidos de las versiones anteriores.

--- app/src/main/java/com/romero/parcial1v5snrm/MainActivity.java
+++ app/src/main/java/com/romero/parcial1v5snrm/MainActivity.java
@@ -1,6 +1,8 @@
 package com.romero.parcial1v5snrm;
 
 import android.content.pm.ActivityInfo;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -39,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
 
         adapter =new ContactsAdapter(contacts,this);
         recyclerView.setAdapter(adapter);
+
+        //boton flotante
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
     }
 
